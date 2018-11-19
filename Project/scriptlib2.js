1cdabc66443323aad67fbd3a3154f417dc92ee1b
mandynyt <43102507+mandynyt@users.noreply.github.com>
2018-11-19 14:39:23 +0800

    try
    try

--- Project/scriptlib2.js
+++ Project/scriptlib2.js
@@ -18,14 +18,12 @@ $(document).ready(function(){
     $("button#get_data").click(function() {
         var items = [];
         var i = 0;
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appsgGTkZbQQG52UE/Purchase%20history?api_key=keyDQXt27JtFQ0kXk";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
         var dataSet = [];
         $.getJSON(airtable_read_endpoint, function(result) {
-          console.log('result')
-          console.log(result)
                $.each(result.records, function(key,value) {
                    items = [];
-                       items.push(value.fields.Date_of_purchase);
+                       items.push(value.fields.Name);
                        items.push(value.fields.Stage_Desc);
                        items.push(value.fields.Completed);
                        items.push(value.fields.Time_Estimate);
@@ -60,31 +58,19 @@ $(document).ready(function(){
      $("button#roll_up").click(function() {
        var table1_items = [];
        var i = 0;
-       var airtable_read_endpoint = "https://api.airtable.com/v0/appsgGTkZbQQG52UE/Purchase%20history?api_key=keyDQXt27JtFQ0kXk";
+       var airtable_read_endpoint = "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
        var table1_dataSet = [];
        $.getJSON(airtable_read_endpoint, function(result) {
-         console.log('result')
-         //console.log(result)
-
-         console.log(JSON.stringify(result))
               $.each(result.records, function(key,value) {
                   table1_items = [];
-                      table1_items.push(value.fields.Product_name);
-                      table1_items.push(value.fields.Date_of_purchase);
-                      if (value.fields.Photo!=undefined) {
-                        table1_items.push("<img src='"+value.fields.Photo[0].url+"' width='24'>");
-                      }
-                      else {
-                        table1_items.push("");
-                      }
-                      table1_items.push(value.fields.Brand);
-                      table1_items.push(value.fields.Product_category);
-                      table1_items.push(value.fields.color);
-                      table1_items.push(value.fields.Selling_price_HKD);
-                      table1_items.push(value.fields.Source);
-                      table1_items.push(value.fields.Repurchase);
+                      table1_items.push(value.fields.Name);
+                      table1_items.push(value.fields.Stage_Desc);
+                      table1_items.push(value.fields.Completed);
+                      table1_items.push(value.fields.Time_Estimate);
+                      table1_items.push(value.fields.Weight_Factor);
+                      table1_items.push(value.fields.converted);
                       table1_dataSet.push(table1_items);
-                      //console.log(table1_items);
+                      console.log(table1_items);
                }); // end .each
                console.log(table1_dataSet);
 
@@ -92,25 +78,18 @@ $(document).ready(function(){
                 data: table1_dataSet,
                 retrieve: true,
                 columns: [
-                    { title: "Product name",
+                    { title: "Name",
                       defaultContent:""},
-                    { title: "Date of purchase",
+                    { title: "Stage",
                         defaultContent:"" },
-                    { title: "Photo",
+                    { title: "Completed",
                       defaultContent:"" },
-                    { title: "Brand",
+                    { title: "Time Estimated",
                       defaultContent:""},
-                    { title: "Product Category",
+                    { title: "Weight Factor",
                         defaultContent:""},
-                    { title: "Color",
+                    { title: "Converted",
                       defaultContent:""},
-                    { title: "Selling price",
-                        defaultContent:""},
-                    { title: "Source",
-                        defaultContent:""},
-                    { title: "Repurchase",
-                        defaultContent:""},
-
                 ]
             } );
        }); // end .getJSON
@@ -118,13 +97,13 @@ $(document).ready(function(){
          var table2_items = [];
          var i = 0;
          var airtable_read_endpoint =
-         "https://api.airtable.com/v0/appsgGTkZbQQG52UE/Purchase%20history?api_key=keyDQXt27JtFQ0kXk";
+         "https://api.airtable.com/v0/appM38HXlEVhxmnqx/Stage?api_key=keyTcsTzckqyBTlk8&view=Grid%20view";
          var table2_dataSet = [];
          $.getJSON(airtable_read_endpoint, function(result) {
                 $.each(result.records, function(key,value) {
                     table2_items = [];
-                        table2_items.push(value.fields.Product_name);
-                        table2_items.push(value.fields.Selling_price_HKD);
+                        table2_items.push(value.fields.Name);
+                        table2_items.push(value.fields.Total_Entries);
                         table2_dataSet.push(table2_items);
                         console.log(table2_items);
                  }); // end .each
@@ -134,17 +113,21 @@ $(document).ready(function(){
                     retrieve: true,
                     ordering: false,
                     columns: [
-                        { title: "Product name",
+                        { title: "Name",
                           defaultContent:""},
-                        { title: "Selling price HKD",
+                        { title: "Total Entries",
                           defaultContent:""},
                     ] // rmf columns
                 } ); // end dataTable
 
                 var chart = c3.generate({
                      data: {
-                         columns: table1_dataSet,
+                         columns: table2_dataSet,
                          type : 'bar'
+                     },
+                     axis: {
+                       x: {label: 'Stage'},
+                       y: {label: '# of Entries'}
                      },
                      bar: {
                          title: "Tasks for Each Stage:",
