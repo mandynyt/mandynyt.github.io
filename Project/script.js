e090e304541ff6e9baa8182fa03a477321164484
mandynyt <43102507+mandynyt@users.noreply.github.com>
2018-11-19 12:01:39 +0800

    update
    update

--- Project/script.js
+++ Project/script.js
@@ -1,33 +1,22 @@
+
 $("button#roll_up").click(function() {
   var table1_items = [];
   var i = 0;
   var airtable_read_endpoint = "https://api.airtable.com/v0/appsgGTkZbQQG52UE/Purchase%20history?api_key=keyDQXt27JtFQ0kXk";
   var table1_dataSet = [];
   $.getJSON(airtable_read_endpoint, function(result) {
-    console.log('result')
-    //console.log(result)
-
-    console.log(JSON.stringify(result))
          $.each(result.records, function(key,value) {
              table1_items = [];
                  table1_items.push(value.fields.Product_name);
                  table1_items.push(value.fields.Date_of_purchase);
-                 if (value.fields.Photo!=undefined) {
-                   table1_items.push("<img src='"+value.fields.Photo[0].url+"' width='24'>");
-                 }
-                 else {
-                   table1_items.push("");
-                 }
+                  table1_items.push('<td><img src="' + value.fields.Photo + '" style="width: 90px;"/></td>');
                  table1_items.push(value.fields.Brand);
                  table1_items.push(value.fields.Product_category);
                  table1_items.push(value.fields.color);
                  table1_items.push(value.fields.Selling_price_HKD);
-                 table1_items.push(value.fields.Source);
-                 table1_items.push(value.fields.Repurchase);
                  table1_dataSet.push(table1_items);
-                 //console.log(table1_items);
           }); // end .each
-          console.log(table1_dataSet);
+
 
        $('#table1').DataTable( {
            data: table1_dataSet,
@@ -46,10 +35,6 @@ $("button#roll_up").click(function() {
                { title: "Color",
                  defaultContent:""},
                { title: "Selling price",
-                   defaultContent:""},
-               { title: "Source",
-                   defaultContent:""},
-               { title: "Repurchase",
                    defaultContent:""},
 
            ]
@@ -70,31 +55,23 @@ $("button#roll_up").click(function() {
                    console.log(table2_items);
             }); // end .each
             console.log(table2_dataSet);
-           $('#table2').DataTable( {
-               data: table2_dataSet,
-               retrieve: true,
-               ordering: false,
-               columns: [
-                   { title: "Product name",
-                     defaultContent:""},
-                   { title: "Selling price HKD",
-                     defaultContent:""},
-               ] // rmf columns
-           } ); // end dataTable
+
 
            var chart = c3.generate({
                 data: {
-                    columns: table1_dataSet,
+                    columns: table2_dataSet,
                     type : 'bar'
                 },
-                bar: {
-                    title: "Tasks for Each Stage:",
+                axis: {
+                    x: {
+                        label: 'Product'
+                    },
+                    y: {
+                        label: 'Selling price'
+                    },
                 }
             });
 
      }); // end .getJSON
   }); // end button
-
-   // $.getJSON('http://localhost/d756a/data_export.json/Computer+TV', function(obj) {
-
 }); // document ready
