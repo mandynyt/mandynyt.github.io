f3df49c91b7406d95110ce23da0a174934f4aa6f
mandynyt <43102507+mandynyt@users.noreply.github.com>
2018-11-28 11:39:13 +0800

    update
    update

--- Project/scriptlib2.js
+++ Project/scriptlib2.js
@@ -139,7 +139,7 @@ $(document).ready(function() {
         },
         bar: {
           title: "Number of students in different Universities",
-          width: { ratio: 1.0 }// this makes bar width 50% of length between ticks
+          width: { ratio: 0.5 }// this makes bar width 50% of length between ticks
         }
       }); // end chart
     }); // end .getJSON
